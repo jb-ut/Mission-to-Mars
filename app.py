56a8a8252b12d6dd026028558f5b0f666d19ee76
jb-ut <78518451+jb-ut@users.noreply.github.com>
2021-05-16 17:46:46 -0500

    Uploading files

---
+++ app.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template, redirect
+from flask_pymongo import PyMongo
+import scrape_mars
+
+app = Flask(__name__)
+
+app.config["MONGO_URI"] = "mongodb://localhost:27017/mars_app"
+mongo = PyMongo(app)
+
+@app.route("/")
+def echo():
+    scraped_data = mongo.db.scraped_data.find_one()
+    return render_template("index.html", scraped_data=scraped_data)
+
+@app.route("/scrape")
+def scraper():
+    scraped_data = mongo.db.scraped_data
+    data = scrape_mars.scrape()
+    scraped_data.update({}, data, upsert=True)
+    return redirect("/", code=302)
+
+if __name__ == "__main__":
+    app.run(debug=True)
